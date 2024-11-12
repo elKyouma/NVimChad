b39908d6fa0852b8636e234dc14ddf985265bc3d
Adrian Gliźniewicz <adrian2402glizniewicz@gmail.com>
2024-11-13 00:59:44 +0100

    Support for python

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -6,16 +6,11 @@
 local M = {}
 
 M.base46 = {
-	theme = "onedark",
+	theme = "chocolate",
 
 	hl_override = {
 		Comment = { italic = true },
 		["@comment"] = { italic = true },
 	},
 }
-
-M.nvdash = {
-  load_on_startup = true,
-}
-
 return M
--- lua/options.lua
+++ lua/options.lua
@@ -3,5 +3,7 @@ require "nvchad.options"
 -- add yours here!
 
 local o = vim.o
-o.cursorlineopt ='both' -- to enable cursorline!
-o.relativenumber =true
+o.cursorlineopt = 'both' -- to enable cursorline!
+o.relativenumber = true
+o.shiftwidth = 4
+o.tabstop = 4
