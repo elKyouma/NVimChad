cfcfa57eb5e03cba662f85b439416d92d7efec25
Adrian Gliźniewicz <adrian2402glizniewicz@gmail.com>
2024-11-12 21:31:12 +0100

    Support cpp with clangd and add space W binding for windows

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,5 +1,5 @@
 -- This file needs to have same structure as nvconfig.lua 
--- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
+-- https://gi:thub.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
 -- Please read that file to know all available options :( 
 
 ---@type ChadrcConfig
@@ -8,10 +8,14 @@ local M = {}
 M.base46 = {
 	theme = "onedark",
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+	hl_override = {
+		Comment = { italic = true },
+		["@comment"] = { italic = true },
+	},
+}
+
+M.nvdash = {
+  load_on_startup = true,
 }
 
 return M
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -2,9 +2,32 @@ require "nvchad.mappings"
 
 -- add yours here
 
-local map = vim.keymap.set
+local mapset = vim.keymap.set
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
+mapset("n", ";", ":", { desc = "CMD enter command mode" })
+mapset("i", "jk", "<ESC>")
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+-- General window commands
+mapset("n", "<leader>w", "<C-w>", { desc = "Window prefix (Space+w)" })  -- For commands like Space+wh, Space+wj, etc.
+mapset("n", "<leader>wh", "<C-w>h", { desc = "Move to left window" })
+mapset("n", "<leader>wj", "<C-w>j", { desc = "Move to bottom window" })
+mapset("n", "<leader>wk", "<C-w>k", { desc = "Move to top window" })
+mapset("n", "<leader>wl", "<C-w>l", { desc = "Move to right window" })
+
+-- Window resizing
+mapset("n", "<leader>w+", "<C-w>+", { desc = "Increase window height" })
+mapset("n", "<leader>w-", "<C-w>-", { desc = "Decrease window height" })
+mapset("n", "<leader>w>", "<C-w>>", { desc = "Increase window width" })
+mapset("n", "<leader>w<", "<C-w><", { desc = "Decrease window width" })
+
+-- Splits
+mapset("n", "<leader>ws", "<C-w>s", { desc = "Horizontal split" })
+mapset("n", "<leader>wv", "<C-w>v", { desc = "Vertical split" })
+
+-- Window closing and managing
+mapset("n", "<leader>wc", "<C-w>c", { desc = "Close current window" })
+mapset("n", "<leader>wo", "<C-w>o", { desc = "Close all other windows" })
+mapset("n", "<leader>wt", "<C-w>t", { desc = "Move to top window" })
+mapset("n", "<leader>wb", "<C-w>b", { desc = "Move to bottom window" })
+
